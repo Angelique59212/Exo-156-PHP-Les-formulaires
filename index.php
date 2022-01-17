e320d42369bdd92b7e405c18c57a7fc2eeea933e
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-17 16:57:46 +0100

    formulaire

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mini Projet</title>
+</head>
+<body>
+    <form action="form.php" method="get">
+        <label for="firstName"></label>
+        <input type="text" id="firstName" name="firstName" placeholder="Votre prénom">
+        <input type="submit" id="submit">
+    </form>
+
+</body>
+</html>
